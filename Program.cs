3f3a60e056fc075c2a59443d875086d3803c3399
Caspian2 <casa04001@utb.vaxjo.se>
2022-12-02 21:42:55 +0100

    Bra upPgift

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Skriv ett nummer!");
+        
+        int nummer;
+        string siffra = Console.ReadLine();
+
+        bool isNumber = int.TryParse(siffra, out nummer);
+
+        Console.WriteLine("Skrev du ett nummer: " + isNumber);
+        
+    }
+
+}
